7e1a2a01e6b8caeb12af8c1c1595d54f9b88982e
Manoj Sethi <manojfullstackdevloper.india@gmail.com>
2025-07-11 19:02:53 +0530

    Recommendation form mobile device responisevenes

--- src/app/components/recommendation/recommendationForm/index.tsx
+++ src/app/components/recommendation/recommendationForm/index.tsx
@@ -18,7 +18,7 @@ const RecommendationForm = ({
   return (
     <Form layout="vertical" name="insuranceForm" onFinish={handleOnFinish}>
       <Row gutter={25}>
-        <Col sm={24} md={12}>
+        <Col span={24} md={12}>
           <label className={styles.label}>Age</label>
           <Form.Item
             name="age"
@@ -41,7 +41,7 @@ const RecommendationForm = ({
             />
           </Form.Item>
         </Col>
-        <Col sm={12} md={12}>
+        <Col span={24} md={12}>
           <label className={styles.label}>Income</label>
           <Form.Item
             name="income"
@@ -64,7 +64,7 @@ const RecommendationForm = ({
             />
           </Form.Item>
         </Col>
-        <Col sm={12} md={12}>
+        <Col span={24} md={12}>
           <label className={styles.label}>Dependents</label>
           <Form.Item
             name="dependents"
@@ -87,9 +87,8 @@ const RecommendationForm = ({
             />
           </Form.Item>
         </Col>
-        <Col sm={12} md={12}>
+        <Col span={24} md={12}>
           <label className={styles.label}>Risk of Tolerance</label>
-
           <Form.Item
             name="riskTolerance"
             hasFeedback
